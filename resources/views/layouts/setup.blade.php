7db56a3ff9dab7e29a5099df72a75897be7381b5
yasaswikalle <76928667+yasaswikalle@users.noreply.github.com>
2023-01-04 13:48:39 -0500

    Test Run

--- resources/views/layouts/setup.blade.php
+++ resources/views/layouts/setup.blade.php
@@ -57,7 +57,7 @@
           <div class="container">
               <div class="row">
                   <div class="col-lg-10 col-lg-offset-1">
-                    <h1 class="page-header">Snipe-IT Pre-Flight</h1>
+                    <h1 class="page-header">Yasaswi</h1>
                   </div>
                   <div class="col-lg-11 col-lg-offset-1">
 
